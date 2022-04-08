15fde36bde92585af9d32f221d607b74b8493fcc
Ricardo Alvarez <ricardo.alvarezr94@gmail.com>
2022-04-07 23:55:49 -0300

    Se agrega ejercicio 2

--- adm_c16/Core/Src/main.c
+++ adm_c16/Core/Src/main.c
@@ -77,6 +77,14 @@ void zeros (uint32_t * vector, uint32_t longitud)
 	}
 }
 
+/* Ejercicio 2 */
+void productoEscalar32 (uint32_t * vectorIn, uint32_t * vectorOut, uint32_t longitud, uint32_t escalar)
+{
+	for(int i = 0; i < longitud; i++)
+	{
+		vectorOut[i] = vectorIn[i] * escalar;
+	}
+}
 
 static void PrivilegiosSVC (void)
 {
