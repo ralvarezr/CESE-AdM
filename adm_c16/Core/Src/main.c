efbcace282a043a2e26e7021a3f85dce3be4853b
Ricardo Alvarez <ricardo.alvarezr94@gmail.com>
2022-04-12 12:51:16 -0300

    Se arregla el nombre de la funcion del ejercicio 4

--- adm_c16/Core/Src/main.c
+++ adm_c16/Core/Src/main.c
@@ -96,7 +96,7 @@ void productoEscalar16 (uint16_t * vectorIn, uint16_t * vectorOut, uint32_t long
 }
 
 /* Ejercicio 4 */
-void productoEscalar16 (uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitud, uint16_t escalar)
+void productoEscalar12 (uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitud, uint16_t escalar)
 {
 	for(uint32_t i = 0; i < longitud; i++)
 	{
